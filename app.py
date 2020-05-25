c5cfba13c4b78815b755de81565fc37cf0c23ce9
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2020-05-25 19:08:13 +0000

    Updating app.py for heroku deployment

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from os import path
-if path.exists("env.py"):
-  import env
+
 
 app = Flask(__name__)
 
--- static/math.js
+++
@@ -1,3 +0,0 @@
-
-const MONGO_URI = env.MONGO_URI
-console.log(MONGO_URI) 
