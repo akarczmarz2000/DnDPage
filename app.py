ea1802bc3a027d5db44cc25ebb38f01ad823699c
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2020-05-27 02:26:34 +0000

    New templates added and routed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
 
@@ -11,9 +11,27 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 @app.route("/")
-@app.route("/character")
+@app.route("/entry")
 def character():
-    return render_template("character.html", char=mongo.db.characters.find())
+    return render_template("entry.html")
+
+
+@app.route("/username", method=["POST"])
+def username(user):
+    user = request.form["name"]
+    username = mongo.db.characters
+    username.mongo.db.characters.find({"user": user}, function (err, user){
+        if (err) {
+            console.log(err)
+        };
+        if (!user.length){
+            return render_template("new_character.html", 
+            classes=mongo.db.classes.find(), race=mongo.db.race.findOne({"user": user}))
+        }
+        else{
+            return render_template("character.html", char=mongo.db.characters.find("user"))
+        }
+    });
 
 
 if __name__ == "__main__":
